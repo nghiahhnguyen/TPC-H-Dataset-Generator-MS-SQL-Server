35f28da2bc9681f9d59a04fedfbe00774e211e41
Nghia <huunghia160799@gmail.com>
2020-07-15 12:28:21 +1000

    Add filtered template number 3

---
+++ dbgen/queries_filtered_2/12.sql
@@ -0,0 +1,39 @@
+-- $ID$
+-- TPC-H/TPC-R Shipping Modes and Order Priority Query (Q12)
+-- Functional Query Definition
+-- Approved February 1998
+:x
+:o
+
+set showplan_all on;
+go
+
+select
+	l_shipmode,
+	sum(case
+		when o_orderpriority = '1-URGENT'
+			or o_orderpriority = '2-HIGH'
+			then 1
+		else 0
+	end) as high_line_count,
+	sum(case
+		when o_orderpriority <> '1-URGENT'
+			and o_orderpriority <> '2-HIGH'
+			then 1
+		else 0
+	end) as low_line_count
+from
+	orders,
+	lineitem
+where
+	o_totalprice > 1000 and
+	o_orderkey = l_orderkey
+	and l_shipmode in (':1', ':2')
+	and l_commitdate < l_receiptdate
+	and l_shipdate < l_commitdate
+	and l_receiptdate >= cast(':3' as datetime)
+	and l_receiptdate < dateadd(mm, 1, cast(':3' as datetime))
+group by
+	l_shipmode
+order by
+	l_shipmode;
---
+++ dbgen/queries_filtered_2/8.sql
@@ -0,0 +1,48 @@
+-- $ID$
+-- TPC-H/TPC-R National Market Share Query (Q8)
+-- Functional Query Definition
+-- Approved February 1998
+:x
+:o
+set showplan_all on;
+go
+
+select
+	o_year,
+	sum(case
+		when nation = ':1' then volume
+		else 0
+	end) / sum(volume) as mkt_share
+from
+	(
+		select
+			datepart(yy, o_orderdate) as o_year,
+			l_extendedprice * (1 - l_discount) as volume,
+			n2.n_name as nation
+		from
+			part,
+			supplier,
+			lineitem,
+			orders,
+			customer,
+			nation n1,
+			nation n2,
+			region
+		where
+			p_retailprice > 1000 and
+			p_partkey = l_partkey
+			and s_suppkey = l_suppkey
+			and l_orderkey = o_orderkey
+			and o_custkey = c_custkey
+			and c_nationkey = n1.n_nationkey
+			and n1.n_regionkey = r_regionkey
+			and r_name = ':2'
+			and s_nationkey = n2.n_nationkey
+			and o_orderdate between cast('1995-01-01' as datetime) and cast('1996-12-31' as datetime)
+			and p_type = ':3'
+	) as all_nations
+group by
+	o_year
+order by
+	o_year;
+go
