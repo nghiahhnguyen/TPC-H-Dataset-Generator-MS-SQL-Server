cd5acf6569b93f7b2b3f13ea3462bf9104cff368
Nghia <huunghia160799@gmail.com>
2020-09-11 09:01:37 +0700

    Update code to generate showplans for imdbload

--- dataset_generation/gen_equivalent_showplans.py
+++ dataset_generation/gen_equivalent_showplans.py
@@ -3,109 +3,10 @@ import random
 import argparse
 import subprocess
 import random
+import glob
 from pathlib import Path
 
 from .utils import extract_tables_columns
-
-
-def extract_clauses(string):
-    print(f"String:\n {string}\n")
-    if 'where' in string:
-        split_keyword = 'where'
-    else:
-        split_keyword = 'group by'
-
-    stack = []
-    current_word = ''
-    split_position = -1
-    for i, char in enumerate(string):
-        print(f"'{char}'", current_word)
-        if char in [' ', '\t', '\n']:
-            if current_word == split_keyword:
-                print(stack)
-                while stack[-1] != 'select':
-                    stack.pop()
-                stack.pop()
-                if len(stack) == 0:
-                    split_position = i
-                    break
-            if char == ' ' and current_word == 'group':
-                current_word = current_word + char
-            else:
-                current_word = ''
-            # else:
-            #     current_word = current_word + char
-        else:
-            current_word = current_word + char
-        if current_word in ['select', 'from']:
-            stack.append(current_word)
-
-    # print(string.split(split_keyword))
-    # select_from = string.split(split_keyword)[0]
-    select_from = string[:split_position - len(split_keyword)]
-    # print("select_from", select_from)
-    # conditions_clause = split_keyword.join(string.split(split_keyword)[1:])
-    conditions_clause = string[split_position:]
-    # print("where_clause", where_clause)
-    from_clause = 'from'.join(select_from.split('from')[1:])
-    select_clause = select_from.split('from')[0]
-    from_clause = from_clause.strip()
-    return select_from, conditions_clause, select_clause, from_clause, split_keyword
-
-
-def create_filtered_queries(args, input_path, table_column_dict):
-    with open(input_path, 'r') as f:
-        string = f.read()
-        # print(string)
-        query_options = string.split('go')[0]
-        # print(string.split('\ngo\n'))
-        string = '\ngo\n'.join(
-            list(filter(lambda x: len(x.strip()) > 0, string.split('\ngo\n')))[-1:])
-        select_from, where_clause, select_clause, from_clause, split_keyword = extract_clauses(
-            string)
-        # print(f"From Clause: \n{from_clause}")
-        if from_clause[0] == '(':
-            inner_statement = from_clause[1:-1]
-            count = 0
-            stack = []
-            for char in from_clause:
-                if char == '(':
-                    stack.append(char)
-                elif char == ')':
-                    stack.pop()
-                    if len(stack) == 0:
-                        break
-                count += 1
-            inner_statement = from_clause[1:count]
-            print(f"Inner Statement:\n{inner_statement}")
-            inner_select_from, inner_where, _, inner_from, _ = extract_clauses(
-                inner_statement)
-            if ',' in inner_from:
-                tables = inner_from.strip().split(',')
-            else:
-                tables = inner_from.strip().split('left outer join')
-            for table in tables:
-                table = table.strip()
-                for column, column_data_type in table_column_dict[table]:
-                    if column_data_type == "int":
-                        extra_filter_1 = f"{column} > {1000}"
-                        extra_filter_2 = f"{column} < {10}"
-                        from_clause_1 = f"{inner_select_from} where {extra_filter_1} and {inner_where}"
-                        from_clause_2 = f"{inner_select_from} where {extra_filter_2} and {inner_where}"
-                        filtered_query_1 = f"{query_options}\ngo\n{select_clause} from ({from_clause_1}) {split_keyword} {extra_filter_1} and {where_clause}"
-                        filtered_query_2 = f"{query_options}\ngo\n{select_clause} from ({from_clause_2})  {split_keyword}where {extra_filter_2} and {where_clause}"
-                        return filtered_query_1, filtered_query_2
-        tables = from_clause.split(',')
-        for table in tables:
-            table = table.strip()
-            for column, column_data_type in table_column_dict[table]:
-                if column_data_type == "int":
-                    extra_filter_1 = f"{column} > {1000}"
-                    extra_filter_2 = f"{column} < {10}"
-                    filtered_query_1 = f"{query_options}\ngo\n{select_from} where {extra_filter_1} and {where_clause}"
-                    filtered_query_2 = f"{query_options}\ngo\n{select_from} where {extra_filter_2} and {where_clause}"
-                    return filtered_query_1, filtered_query_2
-
 
 def drop_all_indexes(table_column_dict, args):
     count_drop_db_indexes = 0
@@ -136,7 +37,22 @@ def generate_showplans(indices, args, split, table_column_dict, count_db_indexes
                 for i in range(3):
                     run_shell_cmd(args, input_path, output_path, i)
     elif args.dataset == "imdbload":
-        indices = [i + 1 for i in range(33)]
+        for template in indices:
+            template += 1
+            for file_path in glob.glob(f"{args.input_directory}/{template}[a-z]*.sql"):
+                directory = f"../dataset_generation/generated_equivalent_showplans_imdbload/{split}/template_{template}/config_{count_db_indexes}/"
+
+                # extract basename of the file
+                file_name = file_path[file_path.rfind("/")+1:]
+                # remove the extension
+                file_name = file_name[:-4]
+
+                output_path = directory + file_name
+                Path(directory).mkdir(parents=True, exist_ok=True)
+                shell_cmd = f'sqlcmd -S {args.server} -U {args.user} -P {args.password} -d {args.dataset} -i {file_path} -o {output_path}.txt'
+                print(shell_cmd)
+                subprocess.call(shell_cmd, shell=True)
+
     else:
         raise NotImplementedError
 
@@ -183,8 +99,8 @@ if __name__ == "__main__":
     drop_all_indexes(table_column_dict, args)
     # iterate through columns and create index on them
     for table_name, column_list in table_column_dict.items():
-        for column_name, _ in column_list:
-            if column_name == "skip":
+        for column_name, column_data_type in column_list:
+            if column_name == "skip" or column_data_type == "text":
                 continue
             command = f'sqlcmd -S {args.server} -U {args.user} -P {args.password} -d {args.dataset} -Q "create index auto_idx_{count_db_indexes} on {table_name}({column_name});"'
             print(command)
--- dataset_generation/utils.py
+++ dataset_generation/utils.py
@@ -1,3 +1,102 @@
+def extract_clauses(string):
+    print(f"String:\n {string}\n")
+    if 'where' in string:
+        split_keyword = 'where'
+    else:
+        split_keyword = 'group by'
+
+    stack = []
+    current_word = ''
+    split_position = -1
+    for i, char in enumerate(string):
+        print(f"'{char}'", current_word)
+        if char in [' ', '\t', '\n']:
+            if current_word == split_keyword:
+                print(stack)
+                while stack[-1] != 'select':
+                    stack.pop()
+                stack.pop()
+                if len(stack) == 0:
+                    split_position = i
+                    break
+            if char == ' ' and current_word == 'group':
+                current_word = current_word + char
+            else:
+                current_word = ''
+            # else:
+            #     current_word = current_word + char
+        else:
+            current_word = current_word + char
+        if current_word in ['select', 'from']:
+            stack.append(current_word)
+
+    # print(string.split(split_keyword))
+    # select_from = string.split(split_keyword)[0]
+    select_from = string[:split_position - len(split_keyword)]
+    # print("select_from", select_from)
+    # conditions_clause = split_keyword.join(string.split(split_keyword)[1:])
+    conditions_clause = string[split_position:]
+    # print("where_clause", where_clause)
+    from_clause = 'from'.join(select_from.split('from')[1:])
+    select_clause = select_from.split('from')[0]
+    from_clause = from_clause.strip()
+    return select_from, conditions_clause, select_clause, from_clause, split_keyword
+
+
+def create_filtered_queries(args, input_path, table_column_dict):
+    with open(input_path, 'r') as f:
+        string = f.read()
+        # print(string)
+        query_options = string.split('go')[0]
+        # print(string.split('\ngo\n'))
+        string = '\ngo\n'.join(
+            list(filter(lambda x: len(x.strip()) > 0, string.split('\ngo\n')))[-1:])
+        select_from, where_clause, select_clause, from_clause, split_keyword = extract_clauses(
+            string)
+        # print(f"From Clause: \n{from_clause}")
+        if from_clause[0] == '(':
+            inner_statement = from_clause[1:-1]
+            count = 0
+            stack = []
+            for char in from_clause:
+                if char == '(':
+                    stack.append(char)
+                elif char == ')':
+                    stack.pop()
+                    if len(stack) == 0:
+                        break
+                count += 1
+            inner_statement = from_clause[1:count]
+            print(f"Inner Statement:\n{inner_statement}")
+            inner_select_from, inner_where, _, inner_from, _ = extract_clauses(
+                inner_statement)
+            if ',' in inner_from:
+                tables = inner_from.strip().split(',')
+            else:
+                tables = inner_from.strip().split('left outer join')
+            for table in tables:
+                table = table.strip()
+                for column, column_data_type in table_column_dict[table]:
+                    if column_data_type == "int":
+                        extra_filter_1 = f"{column} > {1000}"
+                        extra_filter_2 = f"{column} < {10}"
+                        from_clause_1 = f"{inner_select_from} where {extra_filter_1} and {inner_where}"
+                        from_clause_2 = f"{inner_select_from} where {extra_filter_2} and {inner_where}"
+                        filtered_query_1 = f"{query_options}\ngo\n{select_clause} from ({from_clause_1}) {split_keyword} {extra_filter_1} and {where_clause}"
+                        filtered_query_2 = f"{query_options}\ngo\n{select_clause} from ({from_clause_2})  {split_keyword}where {extra_filter_2} and {where_clause}"
+                        return filtered_query_1, filtered_query_2
+        tables = from_clause.split(',')
+        for table in tables:
+            table = table.strip()
+            for column, column_data_type in table_column_dict[table]:
+                if column_data_type == "int":
+                    extra_filter_1 = f"{column} > {1000}"
+                    extra_filter_2 = f"{column} < {10}"
+                    filtered_query_1 = f"{query_options}\ngo\n{select_from} where {extra_filter_1} and {where_clause}"
+                    filtered_query_2 = f"{query_options}\ngo\n{select_from} where {extra_filter_2} and {where_clause}"
+                    return filtered_query_1, filtered_query_2
+
+
 def extract_table_text(string):
     print(string.split("CREATE TABLE"))
     string = list(string.split("CREATE TABLE"))
@@ -14,8 +113,13 @@ def extract_table_text(string):
         for column_string in table_content.split(","):
             column_strings = column_string.strip().split()
             column_name = column_strings[0]
-            column_type = column_strings[1]
-            table_column_dict[table_header].append((column_name, column_type))
+            column_data_type = column_strings[1]
+            table_column_dict[table_header].append(
+                (column_name, column_data_type))
+    for k, v in table_column_dict.items():
+        print(k)
+        for column in v:
+            print(column)
     return table_column_dict
 
 
@@ -57,4 +161,5 @@ def extract_tables_columns(schema_path, dataset="tpch"):
 
 
 if __name__ == "__main__":
-    print(extract_tables_columns("../schema/imdbload-postgres.sql", "imdbload").keys())
+    print(extract_tables_columns(
+        "../schema/imdbload-postgres.sql", "imdbload").keys())
